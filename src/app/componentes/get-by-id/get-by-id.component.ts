87d37d0e4ac2facc5a10e131aa7e1b4664781593
vahan_kh <vahan.khojikyan@bootcamp-upgrade.com>
2024-04-06 19:18:32 +0200

    primera subida

---
+++ src/app/componentes/get-by-id/get-by-id.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-by-id',
+  templateUrl: './get-by-id.component.html',
+  styleUrls: ['./get-by-id.component.css']
+})
+export class GetByIdComponent {
+
+}
