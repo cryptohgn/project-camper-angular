3523570b8163e29b2b0801adc3621d1f5a6fdead
cryptohgn <cryptohgn@gmail.com>
2024-04-10 18:17:59 +0200

    añadidas las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { AdduserComponent } from './componentes/adduser/adduser.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { AddCampervanComponent } from './componentes/add-campervan/add-campervan.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'adduser', component: AdduserComponent },
+  { path: 'addcampervan', component: AddCampervanComponent },
+  { path: 'login', component: LoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
