587d745747ace2067cf152f06f8ba0ce0676d2a4
Mar García Mayans <margarmayans@gmail.com>
2024-04-21 21:17:49 +0200

    subida con prim css filter

--- src/app/componentes/user/user.component.ts
+++ src/app/componentes/user/user.component.ts
@@ -6,25 +6,25 @@ import { ServiceUsers } from 'src/app/services/serviceUsers.services';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
-
-private serviceUsers = inject(ServiceUsers)
-clave!: User;
-router = inject(Router)
+  private serviceUsers = inject(ServiceUsers);
+  clave!: User;
+  router = inject(Router);
 
   loadCurrentUser(): void {
-    const userFromLocalStorage = this.serviceUsers.getCurrentUserFromLocalStorage();
+    const userFromLocalStorage =
+      this.serviceUsers.getCurrentUserFromLocalStorage();
     console.log(userFromLocalStorage);
-    
+
     if (userFromLocalStorage) {
       this.clave = userFromLocalStorage;
     } else {
       this.router.navigate(['/login']);
     }
   }
-  
+
   deleteUser(): void {
     if (this.clave && this.clave.id) {
       const userId = this.clave.id;
@@ -35,22 +35,16 @@ router = inject(Router)
         },
         error: (error: User) => {
           console.error('Error al eliminar usuario:', error);
-        }
+        },
       });
     }
-  };
+  }
 
   editUser(): void {
     this.router.navigate(['/']);
-  };
-
-
-  ngOnInit(): void {
-
-    this.loadCurrentUser();
-
-
   }
 
-
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
 }
